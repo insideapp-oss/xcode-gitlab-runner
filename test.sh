a063997aea92ad14364555c9749458afb36f8700
Gilles Grousset <gilles.grousset@insideapp.fr>
2021-11-16 12:43:21 +0100

    fix: removed test file

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-XCODE_XIP_FILE="~/Downloads/Xcode_13.1.xip" \
-GITLAB_URL="https://gitlab.com/" \
-GITLAB_REGISTRATION_TOKEN="DzyAn6VnpySjQmzp7zZ5" \
-GITLAB_RUNNER_NAME="mac01" \
-GITLAB_RUNNER_TAGS="macos,xcode13" \
-RAM_SIZE=8192 \
-CPU_COUNT=4 \
-vagrant up
